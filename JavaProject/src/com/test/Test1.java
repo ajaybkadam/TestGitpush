5eeb90e3b0166395716be465c6c5a1b7fb7e09f2
info <info@edulab3>
2023-05-17 12:14:42 +0530

    commit my task 17-05-2023

---
+++ JavaProject/src/com/test/Test1.java
@@ -0,0 +1,23 @@
+package com.test;
+
+public class Test1 {
+	int a=100;
+	
+	public void testmethod() {
+		
+		Test1 js=new Test1();
+		js.a=100;
+		System.out.println(a);
+			
+	}
+
+	public static void main(String[] args) {
+
+		Test1 job=new Test1();
+		job.testmethod();
+		
+		
+		
+	}
+
+}
---
+++ JavaProject/src/com/test/Test2.java
@@ -0,0 +1,22 @@
+package com.test;
+
+public class Test2 {
+
+    int b=200;
+	
+	public void testvoidmethod() {
+		
+		Test2 jss=new Test2();
+         jss.b=200;
+		System.out.println(b);
+			
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Test2 key=new Test2();
+	     key.testvoidmethod();
+	
+	}
+
+}
